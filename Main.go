a635f1627976d977d3d2e361e3a81ec591dde619
Brandon <brandonha545@gmail.com>
2026-08-22 12:21:57 -0600

    first commit

---
+++ Main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 17
+	name := "Brandon"
+	const pi = 3.1416
+	suma := 3 + pi
+	fmt.Println("hello world")
+	fmt.Println("Mi nombre es:", name)
+	fmt.Println("Tengo", age, "años")
+	fmt.Println("Pi: ", pi)
+	fmt.Println("Suma: ", suma)
+}
