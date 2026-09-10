394d6e3fd35eb45f63b8fc983e81b009619a0b48
Brandon <brandonha545@gmail.com>
2026-09-09 20:00:24 -0600

    Ejercicio 2.1

--- Main.go
+++ Main.go
@@ -1,15 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var age int = 17
-	name := "Brandon"
-	const pi = 3.1416
-	suma := 3 + pi
-	fmt.Println("hello world")
-	fmt.Println("Mi nombre es:", name)
-	fmt.Println("Tengo", age, "años")
-	fmt.Println("Pi: ", pi)
-	fmt.Println("Suma: ", suma)
+
+	username := "Juan342"
+	email := "mail@gmail.com"
+	password := "12345678"
+	age := 18
+
+	var errors []string
+
+	if len(username) >= 4 && len(username) <= 20 {
+		fmt.Println("Username correcto")
+	} else {
+		errors = append(errors, "Username incorrecto")
+	}
+
+	if strings.Contains(email, "@") && strings.Contains(email, ".") {
+		fmt.Println("Email correcto")
+	} else {
+		errors = append(errors, "Email incorrecto")
+	}
+
+	if len(password) >= 8 {
+		fmt.Println("Password correcto")
+	} else {
+		errors = append(errors, "Password incorrecto")
+	}
+
+	if age >= 18 {
+		fmt.Println("Age correcto")
+	} else {
+		errors = append(errors, "Age incorrecto")
+	}
+
+	if len(errors) == 0 {
+		fmt.Println("Registro exitoso")
+	} else {
+		fmt.Println("Error:")
+		fmt.Println(errors)
+	}
 }
